b9d80138acc5adee412c55b8afc2a6dc4a1ed3df
Frank Xu <frank.xu@northbright.com>
2016-03-12 18:48:02 +0800

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# go-static-file-server-example
+
+A golang static file server example.  
+
+[![Build Status](https://travis-ci.org/northbright/go-static-file-server-example.svg?branch=master)](https://travis-ci.org/northbright/go-static-file-server-example)
+
+## Notes
+* Make sure that the path passed in `http.FileServer(http.Dir("XX"))` is **ABSOLUTE**.  
+* `http.Dir("./static")` works only if `PWD` is the server root dir.
+* Use [GetCurrentExecDir](https://godoc.org/github.com/northbright/pathhelper#GetCurrentExecDir) to get absolute path of server root(current executable).
+
+## References
+* [Serve homepage and static content from root](http://stackoverflow.com/questions/14086063/serve-homepage-and-static-content-from-root)
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"text/template"
+)
+
+var (
+	serverRoot       string // Absolute path of server root
+	staticFolderPath string // Absolute path of static file folder
+	faviconPath      string // Absolute path of "favicon.ico"
+)
+
+// serveSingleFile() serves Single Static File.
+func serveSingleFile(pattern string, filename string) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filename)
+	})
+}
+
+// hello() is home handler.
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	t := template.Must(template.New("hello").Parse(indexHTMLSrc))
+	d := struct {
+		Title   string
+		Content string
+	}{
+		"Hello, World!",
+		"Static File Server Example:-)",
+	}
+
+	if err := t.Execute(w, &d); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// GetCurrentExecDir() gets the current executable path.
+// You may find more path helper functions in:
+// https://github.com/northbright/pathhelper
+func GetCurrentExecDir() (dir string, err error) {
+	p, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+
+	absPath, err := filepath.Abs(p)
+	if err != nil {
+		return "", err
+	}
+
+	dir = filepath.Dir(absPath)
+	return dir, nil
+}
+
+func init() {
+	// Get absolute path of server root(current executable).
+	serverRoot, _ = GetCurrentExecDir()
+	// Get static folder path.
+	staticFolderPath = path.Join(serverRoot, "./static")
+	// Get favicon.ico path.
+	faviconPath = path.Join(serverRoot, "favicon.ico")
+}
+
+func main() {
+	// Index Handler
+	http.HandleFunc("/", hello)
+
+	// Serve Single File(Ex: favicon.ico)
+	serveSingleFile("/favicon.ico", faviconPath)
+
+	// Serve Static Files
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticFolderPath))))
+
+	fmt.Printf("Starting server...\nstatic folder abs path: %v\n", staticFolderPath)
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// HTML src of home("/").
+var indexHTMLSrc = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>{{.Title}}</title>
+  <meta charset="UTF-8" />
+  <link rel='stylesheet' id='style-css'  href='static/css/style.css' media='all' />
+  <script type="text/javascript" src="static/js/jquery-1.12.1.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <h1>{{.Content}}</h1>
+  </div>
+</body>
+</html>
+`
